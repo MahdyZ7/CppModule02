227160a8636ade7f2f1291842e3875d818f6c441
Ahmed Yassin <ayassin@lab3r7s10.42abudhabi.ae>
2022-10-04 17:51:53 +0400

    ex01 done

---
+++ ex02/Fixed.hpp
@@ -0,0 +1,54 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Fixed.hpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ayassin <ayassin@student.42abudhabi.ae>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/09/27 16:09:01 by ayassin           #+#    #+#             */
+/*   Updated: 2022/10/04 17:44:46 by ayassin          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FIXED_HPP
+#define FIXED_HPP
+
+#include <iostream>
+
+class Fixed{
+	public:
+		Fixed(void);
+		Fixed(const int x);
+		Fixed(const float x);
+		Fixed(Fixed const &src);
+		~Fixed(void);
+
+		Fixed &operator=(Fixed const &cpy);
+		Fixed &operator+(Fixed const &cpy);
+		Fixed &operator-(Fixed const &cpy);
+		Fixed &operator*(Fixed const &cpy);
+
+		Fixed operator++(); //prefix
+		Fixed operator++(int); //postfix
+		Fixed operator--(); //prefix
+		Fixed operator--(int); //postfix
+
+		bool &operator>(Fixed const &cpy);
+		bool &operator>=(Fixed const &cpy);
+		bool &operator<(Fixed const &cpy);
+		bool &operator<=(Fixed const &cpy);
+		bool &operator==(Fixed const &cpy);
+		bool &operator!=(Fixed const &cpy);
+		
+		int		getRawBits( void ) const;
+		void	setRawBits( int const raw );
+		float	toFloat(void) const;
+		int		toInt(void) const;
+		private:
+			int num;
+			static const int frac = 8;
+};
+
+std::ostream& operator<<(std::ostream &os, Fixed const &n);
+
+#endif 
